55a49d09106311e7561229f7421a89b7888de336
Ethan R. Jones <doubleplusgood23@gmail.com>
2014-10-02 21:06:21 -0400

    Adding ch4p27

---
+++ sets/ch4/ch4p27/ch4p27.java
@@ -0,0 +1,16 @@
+/*
+Ethan R. Jones
+10-2-14
+Chp4 P27
+*/
+
+public class ch4p27
+{
+    public static void main(String args [])
+    {
+	for(int x = 1; 0 == 0; x++)
+	    {
+		System.out.println("" + x * 2);
+	    }
+    } 
+}
