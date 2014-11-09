a485485d26395174a59c9fd572cbc15d1e78d0bf
Ethan R. Jones <doubleplusgood23@gmail.com>
2014-11-08 22:09:39 -0500

    Hanoi set. VERY simple, took me too long

--- sets/ch6/ch6p37/ch6p37.java
+++ sets/ch6/ch6p37/ch6p37.java
@@ -29,28 +29,25 @@ public class ch6p37 extends JApplet implements ActionListener // ActionListener 
 	form1.add(oArea);
     }
 
-    // tower function, tower(Number of disks, Intinal peg, Disks being move to peg, Temporary holding peg)
-    public static String towers(int nDisks, int fromPeg, int toPeg, int tempPeg)
+    // tower function, this took me like 3 hours at this point. KILL ME
+
+    public void towers(int nDisks, int fromPeg, int tempPeg, int toPeg)
     {
-	String s1, s2, s3;
-	
-	if(nDisks == 1)
+	if(nDisks == 0)
 	    {
-		return fromPeg + " → " + toPeg + "\n";
+		return;
 	    }
 	else
 	    {
-		tempPeg = 6 - fromPeg - toPeg;
-		s1 = towers(nDisks - 1, fromPeg, tempPeg, toPeg);
-		s2 = fromPeg + " → " + toPeg + "\n";
-		s3 = towers(nDisks - 1, tempPeg, fromPeg, toPeg);
+		towers(nDisks - 1, fromPeg, toPeg, tempPeg);
+		oArea.append("" + fromPeg + " -> " + toPeg + "\n");
+		towers(nDisks - 1, tempPeg, fromPeg, toPeg);
 	    }
-	return s1 + s2 + s3;
     }
-	
+    
     public void actionPerformed(ActionEvent e)
     {
 	// Call tower() with user's input from iField.
-	oArea.append(towers(Integer.parseInt(iField.getText()), 1, 3, 2));
+	towers(Integer.parseInt(iField.getText()), 1, 2, 3);
     }
 }
