62dfb1160d4fb2441f9b93cca2d4f3ca824bb59e
Ethan R. Jones <doubleplusgood23@gmail.com>
2014-12-16 10:21:27 -0500

    Make middle finds the two middle numbers in a even length number.

---
+++ sets/ch7/makeMiddle.java
@@ -0,0 +1,11 @@
+/*
+  Ethan R. Jones
+  12/15/14
+  Make Middle Codingbat
+*/
+public int[] makeMiddle(int[] nums)
+{
+    int[] mid = {nums[(nums.length - 1) / 2], nums[(nums.length) / 2]};
+    
+    return mid;
+}
