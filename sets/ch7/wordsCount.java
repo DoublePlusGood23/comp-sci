d832b822d87d6b951d8aa7100e2c16290bfda2cf
Ethan R. Jones <doubleplusgood23@gmail.com>
2014-12-18 09:50:12 -0500

    Simple words count from CodingBat.

--- sets/ch7/wordsCount.java
+++ sets/ch7/wordsCount.java
@@ -1,6 +1,19 @@
 /*
   Ethan R. Jones
-  12/16/14
+  12/18/14
   wordsCount
 */
+public int wordsCount(String[] words, int len)
+{
+    int count = 0;
+    for(int x = 0; x < words.length; x++)
+	{
+	    if(words[x].length() == len)
+		{
+		    count++;
+		}
+	}
+    return count;  
+}
+
 
