9bb7bcd2609cfcb65bca72e1bf1eab7248e5434a
Ethan R. Jones <doubleplusgood23@gmail.com>
2014-12-18 10:12:06 -0500

    'Creative' wordsWithout codingbat solution

--- sets/ch7/wordsWithout.java
+++ sets/ch7/wordsWithout.java
@@ -1,5 +1,28 @@
 /*
   Ethan R. Jones
-  12/16/14
+  12/18/14
   Words without
 */
+public String[] wordsWithout(String[] words, String target)
+{
+    int stringSize = 0;
+    int n = 0;
+    for(int x = 0; x < words.length; x++)
+	{
+	    if(words[x] != target)
+		{
+		    stringSize++;
+		}
+	}
+    String[] newWords = new String[stringSize];
+    for(int x = 0; x < words.length; x++)
+	{
+	    if(words[x] != target)
+		{
+		    newWords[n] = words[x];
+		    n++;
+		}
+	}
+    return newWords;   
+}
+
