b33736a02cf9fbdf4bbed3d398bb3eed3cd5e6c6
Ethan R. Jones <doubleplusgood23@gmail.com>
2014-12-21 15:59:57 -0500

    LinearIn solution (about damn time)

--- sets/ch7/linearln.java
+++ sets/ch7/linearln.java
@@ -3,3 +3,30 @@
   12/16/14
   Linear Line
 */
+public boolean linearIn(int[] outer, int[] inner)
+{
+    int numFound = 0;
+    if(inner.length == 0)
+	{
+	    return true;
+	}
+
+    int q = 0;
+    for(int x = 0; x < outer.length; x++)
+	{
+	    if(outer[x] == inner[q])
+		{
+		    numFound++;
+		    q++;
+		}
+	    else if(outer[x] > inner[q])
+		{
+		    return false;
+		}
+
+	    if(numFound == inner.length)
+		return true;
+	}
+
+    return false;
+}
