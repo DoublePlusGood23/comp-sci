dfb3203b57f632155e5021c95cbe2a305d98fd00
Ethan R. Jones <doubleplusgood23@gmail.com>
2014-12-21 16:00:23 -0500

    Satan made this problem.

--- sets/ch7/twoTwo.java
+++ sets/ch7/twoTwo.java
@@ -3,3 +3,17 @@
   12/16/14
   twoTwo
 */
+public boolean twoTwo(int[] nums)
+{
+    for (int x = 0; x < nums.length; x++)
+	{
+	    if (nums[x] != 2)
+		continue;
+	    if (x >= 1 && nums[x - 1] == 2)
+		continue;
+	    if (x < (nums.length - 1) && nums[x + 1] == 2)
+		continue;
+	    return false;
+	}
+    return true;
+}
