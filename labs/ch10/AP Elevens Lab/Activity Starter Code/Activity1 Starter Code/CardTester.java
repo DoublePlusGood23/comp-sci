3b4a3309d40b6b871d242bac807786d97fcf793e
Ethan R. Jones <doubleplusgood23@gmail.com>
2015-03-16 10:23:33 -0400

    Act1 done.

--- labs/ch10/AP Elevens Lab/Activity Starter Code/Activity1 Starter Code/CardTester.java
+++ labs/ch10/AP Elevens Lab/Activity Starter Code/Activity1 Starter Code/CardTester.java
@@ -1,13 +1,26 @@
 /**
  * This is a class that tests the Card class.
  */
-public class CardTester {
+public class CardTester
+{
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args)
+    {
+	Card aceClubs1 = new Card("ace", "clubs", 1);
+	Card aceClubs2 = new Card("ace", "clubs", 1);
+	Card sixHearts = new Card("6", "hearts", 6);
+
+	System.out.println("**** ace of clubs *****");
+	System.out.println("  rank: " + aceClubs1.rank());
+	System.out.println("  suit: " + aceClubs1.suit());
+	System.out.println("  pointValue: " + aceClubs1.pointValue());
+	System.out.println("\n**** clubs2 ****");
+	System.out.println("  rank: " + aceClubs2.rank());
+	System.out.println("  suit: " + aceClubs2.suit());
+	System.out.println("  pointValue: " + aceClubs2.pointValue());
+    }
 }
